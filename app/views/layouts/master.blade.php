cb3ca30529b8ddb4fc83c44d0340c6dade3acb94
Arun <arunr@acube.co>
2015-01-21 13:44:44 +0530

    initial commit

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            @section('title')
+            Laravel 4 - Tutorial
+            @show
+        </title>
+
+        <!-- CSS are placed here -->
+
+    </head>
+
+    <body>
+        <!-- Container -->
+        <div class="container">
+
+            <!-- Content -->
+            @yield('content')
+
+        </div>
+
+        <!-- Scripts are placed here -->
+
+    </body>
+</html>
